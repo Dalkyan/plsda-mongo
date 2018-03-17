760c4eaec47b5ae07237afb171e89d16d0770231
lukbacic@gmail.com <lukbacic@gmail.com>
2018-03-17 14:31:00 +0100

    polaczenie do bazy

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,6 +6,10 @@ import com.mongodb.MongoClientURI;
 import com.mongodb.ServerAddress;
 import com.mongodb.MongoCredential;
 import com.mongodb.MongoClientOptions;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
 
 import java.util.Arrays;
@@ -15,14 +19,33 @@ public class App {
     public static void main(String[] args){
 
         String user ="sda"; // the user name
-        String database = "admin"; // the name of the database in which the user is defined
+        String databaseName = "admin"; // the name of the database in which the user is defined
         char[] password = "sda".toCharArray(); // the password as a character array
 
-        MongoCredential credential = MongoCredential.createCredential(user, database, password);
+
+
+        MongoCredential credential = MongoCredential.createCredential(user, databaseName, password);
         MongoClientOptions options = MongoClientOptions.builder().sslEnabled(true).build();
-        MongoClient mongoClient = new MongoClient(new ServerAddress("cluster0-shard-00-00-eos78.mongodb.net", 27017),
+        MongoClient mongoClient = new MongoClient(  Arrays.asList(
+                new ServerAddress("cluster0-shard-00-00-eos78.mongodb.net", 27017),
+                new ServerAddress("cluster0-shard-00-01-eos78.mongodb.net", 27017),
+                new ServerAddress(" cluster0-shard-00-02-eos78.mongodb.net", 27017)),
                 Arrays.asList(credential),
                 options);
+
+        MongoDatabase database = mongoClient.getDatabase("test");
+        MongoCollection<Document> coll = database.getCollection("restaurants");
+
+        MongoCursor<Document> iterator = coll.find().iterator();
+
+        while (iterator.hasNext() ){
+            String json = iterator.next().toJson();
+            System.out.println(json);
+        }
+
+        mongoClient.close();
+
+
 
 
 
